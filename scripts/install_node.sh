ff30277cdb0f1e749984ded7e6774ca2c8f6d86c
Daniel J. Hofmann <daniel@trvx.org>
2016-10-18 14:05:20 -0700

    Makes Scripts Fail Fast and Loud

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+# set -o nounset
+# disable for sourcing nvm
+
+
 # here we set up the node version on the fly. currently only node 4, but can be used for more values if need be
 # This is done manually so that the build works the same on OS X
 rm -rf ~/.nvm/ && git clone --depth 1 --branch v0.30.1 https://github.com/creationix/nvm.git ~/.nvm
